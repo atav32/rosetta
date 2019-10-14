bc079e40753ffe5ef746af647ede24884d6280a3
Brian Zhang <brianbzhang@gmail.com>
2019-10-14 08:42:25 -0700

    add initial scratch files to play around with

---
+++ scratch/scratch.js
@@ -0,0 +1,12 @@
+console.log('scratch')
+
+const foo_en = (a, b, c) => {
+  return a + b + c;
+};
+
+const foo_zh = (它, 她, 他) => {
+  return 它 + 她 + 他;
+};
+
+console.log(foo_en(1, 2, 3));
+console.log(foo_zh(1, 2, 3));
